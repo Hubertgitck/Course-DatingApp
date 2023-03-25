88d066c36aca371edb891d9c9203a0e82c1e0dd5
Hubertgitck <103528563+Hubertgitck@users.noreply.github.com>
2023-03-25 13:47:07 +0100

    Backend skeleton done

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,13 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
