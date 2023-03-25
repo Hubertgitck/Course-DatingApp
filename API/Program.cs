fe2b76ff383c2f1c76a899f7e057c3e502551a08
Hubertgitck <103528563+Hubertgitck@users.noreply.github.com>
2023-03-25 15:15:57 +0100

    added front-end Angular

--- API/Program.cs
+++ API/Program.cs
@@ -6,11 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-
+builder.Services.AddCors();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseCors(policyBuilder => policyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
